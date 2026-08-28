af747ce4a6624773e132a88a1b4e0f55e8ee21c1
Daniele Lacamera <root@danielinux.net>
2026-08-28 13:08:33 +0200

    test: cover the wolfIP_register_l2_handler stub
    autocov enforces 100% function coverage of src/wolfip.c and the stub added
    with the declaration had no caller in the unit build. The test also checks it
    leaves the EAPOL handler alone, since the two sit next to each other.

--- src/test/unit/unit.c
+++ src/test/unit/unit.c
@@ -367,6 +367,7 @@ Suite *wolf_suite(void)
     tcase_add_test(tc_utils, test_sock_getsockname_icmp_success);
     tcase_add_test(tc_utils, test_register_callback_variants);
     tcase_add_test(tc_utils, test_register_eapol_handler);
+    tcase_add_test(tc_utils, test_register_l2_handler_reports_not_implemented);
     tcase_add_test(tc_utils, test_eapol_handler_dispatch);
     tcase_add_test(tc_utils, test_sock_connect_udp_bound_ip_not_local);
     tcase_add_test(tc_utils, test_sock_connect_udp_bound_ip_success);
